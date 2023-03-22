dbe57d1546e8e4c1bd1982f04a4cf1afd3ac7e66
Davi <davi.frrs@gmail.com>
2023-03-22 10:02:42 -0300

    feat: created hero page

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,5 +1,1 @@
-import { Heading } from '@ignite-ui/react'
-
-export default function Home() {
-  return <Heading as="h1">Hello world, you fucking suck</Heading>
-}
+export { default } from './home'
