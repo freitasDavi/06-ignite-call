844e233c7a7c82500fb9b9f18df300011d662dff
Davi <davi.frrs@outlook.com>
2023-04-06 18:06:56 -0300

    feat: validating no available scheduling in a day

--- src/pages/api/users/[username]/blocked-dates.api.ts
+++ src/pages/api/users/[username]/blocked-dates.api.ts
@@ -44,13 +44,28 @@ export default async function handle(
     )
   })
 
-  const blockedDatesRaw = await prisma.$queryRaw`
-    SELECT * 
+  const yearMonth = `${year}-${String(month).padStart(2, '0')}`;
+
+  const blockedDatesRaw: Array<{ date: number }> = await prisma.$queryRaw`
+    SELECT 
+      EXTRACT(DAY FROM s.date) AS date,
+      COUNT(s.date) AS amount,
+      ((uti.time_end_in_minutes - uti.time_start_in_minutes) / 60) AS size
     FROM schedulings s
 
+    LEFT JOIN user_time_intervals uti
+    ON uti.week_day = WEEKDAY(DATE_ADD(s.date, INTERVAL 1 DAY))
+
     WHERE s.user_id = ${user.id}
-      AND DATE_FORMAT(s.date, "%Y-%m") = ${`${year}-${month}`}
+      AND DATE_FORMAT(s.date, "%Y-%m") = ${yearMonth}
+
+    GROUP BY EXTRACT(DAY FROM s.date),
+    ((uti.time_end_in_minutes - uti.time_start_in_minutes) / 60)
+
+    HAVING amount >= size
   `;
 
-  return res.json({ blockedWeekDays, blockedDatesRaw })
+  const blockedDates = blockedDatesRaw.map((item) => item.date)
+
+  return res.json({ blockedWeekDays, blockedDates })
 }
